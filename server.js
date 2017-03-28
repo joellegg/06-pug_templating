c37b71d97e020f44b9ee5e42531c1e17cece0d60
joellegg <joellegg@bellsouth.net>
2017-03-28 11:05:00 -0500

    set up view for container and installed materialize

--- server.js
+++ server.js
@@ -8,12 +8,19 @@ app.use(express.static('public'));
 
 app.set('view engine', 'pug');
 
-const names = ["Larry", "Moe", "Curly", "Frank"]
+const names = ["Larry", "Moe", "Curly", "Frank"];
+
+// ROUTES
 app.get('/', (req, res, next) => {
   // first object is name of the template, second is the object of data
   res.render('index', {subtitle: "This came from my JS data", names, loggedIn: false})
-})
+});
+app.get('/article', (req, res, next) => {
+  res.render('article', {subtitle: "This came from my JS data", names, loggedIn: true})
+});
 
+
+// PORT
 const port = process.env.PORT || 3000
 // this sets the port so we can get it later, may or may not be necessary
 app.set('port', port);
