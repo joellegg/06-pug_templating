0395994c0717530bae1724555043174635345052
joellegg <joellegg@bellsouth.net>
2017-03-28 10:19:15 -0500

    set up pug template for index and added server routing

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+// three routes; `/`, `/about` and `/inventory`
+app.use(express.static('public'));
+
+app.set('view engine', 'pug');
+
+const names = ["Larry", "Moe", "Curly", "Frank"]
+app.get('/', (req, res, next) => {
+  // first object is name of the template, second is the object of data
+  res.render('index', {subtitle: "This came from my JS data", names, loggedIn: false})
+})
+
+const port = process.env.PORT || 3000
+// this sets the port so we can get it later, may or may not be necessary
+app.set('port', port);
+
+app.listen(port, () => {console.log(`listening on port ${port}`)})
